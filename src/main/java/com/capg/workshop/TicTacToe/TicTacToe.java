912185c670fd202fa6047ebe00d5a6d035c03af6
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-08 10:53:45 +0530

    [NISHANT] add : Ability to allow player to choose X or O

--- src/main/java/com/capg/workshop/TicTacToe/TicTacToe.java
+++ src/main/java/com/capg/workshop/TicTacToe/TicTacToe.java
@@ -1,7 +1,12 @@
 package com.capg.workshop.TicTacToe;
 
-public class TicTacToe {
+import java.util.Scanner;
 
+public class TicTacToe {
+   /**
+    * UC1
+    * @return
+    */
 	public static char[] board() {
 		char[] myBoard = new char[10];
 		for (int i = 0; i < 10; i++) {
@@ -9,8 +14,22 @@ public class TicTacToe {
 		}
 		return myBoard;
 	}
+   /**
+    * UC2
+    */
+	public static char letter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Player1,Please enter your choice(X/O):");
+		char choice = sc.next().charAt(0);
+		if (choice == 'X') 
+			choice = 'X';
+		else 
+			choice = 'O';
+		return choice;
+	} 
 
 	public static void main(String[] args) {
 		board();
+		letter();
 	}
 }
